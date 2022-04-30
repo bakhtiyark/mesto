8ec2cd5075a98447835c375a3ef1a08dcf757b96
Bakhtiyar K <bakhtiyark234@gmail.com>
2022-05-01 03:53:49 +0500

    Formatted

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,33 @@
 let profile = document.querySelector('.profile');
 let editButton = profile.querySelector('.profile__button-edit');
 let addButton = profile.querySelector('.profile__button-add');
-let closePopUp = profile.querySelector('.popup__close-icon');
+let submitButton = profile.querySelector(".popup__save-button")
+let closePopUpButton = profile.querySelector('.popup__close-icon');
 let popUp = document.querySelector(".popup")
-
-
-function addButtonPressed() {
-    popUp.style.visibility = 'visible';
-}
-function closeButtonPressed() {
-    popUp.style.visibility = 'hidden';
-}
-
-addButton.addEventListener("click", addButtonPressed);
-closePopUp.addEventListener("click", closeButtonPressed);
-
-
+let profileName = profile.querySelector(".profile__name");
+let profileSecondary = profile.querySelector(".profile__subtitle");
+let profileNameInp = profile.querySelector('.popup__input_profile_name');
+let profileSecondaryInp = profile.querySelector('.popup__input_profile_secondary');
 let profileCard = document.querySelector('.profile__card');
-let profileName = profileCard.querySelector('.profile__name');
 let profileSubtitle = profileCard.querySelector('.profile__subtitle');
 let profileAvatar = profileCard.querySelector('.profile__avatar');
+
+function editButtonPressed() {
+    profileNameInp.value = profileName.textContent;
+    profileSecondaryInp.value = profileSecondary.textContent;
+    popUp.style.visibility = 'visible';
+}
+
+function closePopUp() {
+    popUp.style.visibility = 'hidden';
+}
+function profileCardUpdate(e) {
+    e.preventDefault();    
+    profileName.innerHTML = profileNameInp.value;
+    profileSecondary.innerHTML = profileSecondaryInp.value;
+    closePopUp();
+  }
+
+submitButton.addEventListener("click", profileCardUpdate);
+editButton.addEventListener("click", editButtonPressed);
+closePopUpButton.addEventListener("click", closePopUp);
