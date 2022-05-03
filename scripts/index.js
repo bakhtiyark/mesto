6c7e63861e831725a2e6f6ae587f4e761038af9e
Bakhtiyar K <bakhtiyark234@gmail.com>
2022-05-03 17:49:01 +0500

    JS script update

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 let profile = document.querySelector('.profile');
-let editButton = profile.querySelector('.profile__button-edit');
-let addButton = profile.querySelector('.profile__button-add');
+let editButton = profile.querySelector('.profile__button_edit');
+let addButton = profile.querySelector('.profile__button_add');
 let submitButton = profile.querySelector(".popup__save-button")
 let closePopUpButton = profile.querySelector('.popup__close-icon');
 let popUp = document.querySelector(".popup")
@@ -11,23 +11,24 @@ let profileSecondaryInp = profile.querySelector('.popup__input_profile_secondary
 let profileCard = document.querySelector('.profile__card');
 let profileSubtitle = profileCard.querySelector('.profile__subtitle');
 let profileAvatar = profileCard.querySelector('.profile__avatar');
+let popUpForm = popUp.querySelector(".popup__form")
 
 function editButtonPressed() {
     profileNameInp.value = profileName.textContent;
     profileSecondaryInp.value = profileSecondary.textContent;
-    popUp.style.visibility = 'visible';
+    popUp.classList.add("popup_opened");
 }
 
 function closePopUp() {
-    popUp.style.visibility = 'hidden';
+    popUp.classList.remove("popup_opened");
 }
 function profileCardUpdate(e) {
-    e.preventDefault();    
+    e.preventDefault();
     profileName.innerHTML = profileNameInp.value;
     profileSecondary.innerHTML = profileSecondaryInp.value;
     closePopUp();
-  }
+}
 
-submitButton.addEventListener("click", profileCardUpdate);
+popUpForm.addEventListener("submit", profileCardUpdate);
 editButton.addEventListener("click", editButtonPressed);
 closePopUpButton.addEventListener("click", closePopUp);
